b80700091bb32a9ba30e238c79923c64717cccc7
Franziska von der Goltz <franziska@vdgoltz.eu>
2017-03-16 15:19:32 -0700

    completed part1 'promises' section

---
+++ js/ratefinder.js
@@ -0,0 +1,12 @@
+let url = "rates.json";
+
+fetch(url)
+    .then(response => response.json())
+    .then(rates => {
+        let html = "";
+        rates.forEach(rate => html += 
+            `<tr><td>${rate.name}</td><td>${rate.rate}%</td><tr>
+        `);
+        document.getElementById("rates").innerHTML = html;    
+    })
+    .catch(e => console.log(e));
